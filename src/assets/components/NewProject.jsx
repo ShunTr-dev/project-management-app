656981aca58b29cb2ff9c8f2af31b9f50b42c0d0
ShunTr <repaco9@gmail.com>
2024-10-04 20:12:54 +0200

    Styling Modal

--- src/assets/components/NewProject.jsx
+++ src/assets/components/NewProject.jsx
@@ -29,8 +29,8 @@ export default function NewProject({ onAdd }) {
     return (
         <>
             <Modal ref={modal} buttonCaption="Close">
-                <h2 className="text-2xl font-bold text-stone-800">Invalid input</h2>
-                <p className="text-stone-800">Please fill out all fields.</p>
+                <h2 className="text-xl font-bold text-stone-800 my-4">Invalid input</h2>
+                <p className="text-stone-600 mb-4">Please make sure you provide a valid value for each field.</p>
             </Modal>
             <div className="w-[35rem] mt-16">
                 <menu className="flex items-center justify-end gap-4 my-4">
